fab4396c540789e121a15febd79945e7acb5a5ed
Camilo Restre <camilorestrerodriguez@gmail.com>
2024-05-22 22:02:02 -0500

    feat:  user_page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -95,77 +95,79 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              //*logo
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Image Clicked!')));
-                  },
-                  child: const SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Image(
-                        image: AssetImage('assets/logoByHam.png'),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                //* app name
-                const Text(
-                  'B L A C K L I S T',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                //* username
-                MyTextField(
-                    hintText: 'Tu nombre de usuario',
-                    obscureText: false,
-                    controller: usernamecontroller),
-                const SizedBox(height: 20),
-                //* email
-                MyTextField(
-                    hintText: 'Tu correo',
-                    obscureText: false,
-                    controller: emailcontroller),
-                const SizedBox(height: 20),
-                //* password
-                MyTextField(
-                    hintText: 'Clave 1',
-                    obscureText: true,
-                    controller: passwordcontroller),
-                const SizedBox(height: 20),
-                //* confirm password
-                MyTextField(
-                    hintText: 'Confirma tu clave 1',
-                    obscureText: true,
-                    controller: confirmpasswordcontroller),
-
-                const SizedBox(height: 20),
-                //* Register button
-                Mybutton(text: 'Registrarme', onTap: registeruser),
-
-                const SizedBox(height: 20),
-                //* login button
-                Row(
-                  children: [
-                    const Text('Ya tienes cuenta?'),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: Text('  Inicia Sesión',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).colorScheme.tertiary)),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                //*logo
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Image Clicked!')));
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Image(
+                          image: AssetImage('assets/logoByHam.png'),
+                          fit: BoxFit.cover),
                     ),
-                  ],
-                ),
-              ]),
+                  ),
+                  const SizedBox(height: 20),
+                  //* app name
+                  const Text(
+                    'B L A C K L I S T',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  //* username
+                  MyTextField(
+                      hintText: 'Tu nombre de usuario',
+                      obscureText: false,
+                      controller: usernamecontroller),
+                  const SizedBox(height: 20),
+                  //* email
+                  MyTextField(
+                      hintText: 'Tu correo',
+                      obscureText: false,
+                      controller: emailcontroller),
+                  const SizedBox(height: 20),
+                  //* password
+                  MyTextField(
+                      hintText: 'Clave 1',
+                      obscureText: true,
+                      controller: passwordcontroller),
+                  const SizedBox(height: 20),
+                  //* confirm password
+                  MyTextField(
+                      hintText: 'Confirma tu clave 1',
+                      obscureText: true,
+                      controller: confirmpasswordcontroller),
+
+                  const SizedBox(height: 20),
+                  //* Register button
+                  Mybutton(text: 'Registrarme', onTap: registeruser),
+
+                  const SizedBox(height: 20),
+                  //* login button
+                  Row(
+                    children: [
+                      const Text('Ya tienes cuenta?'),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: Text('  Inicia Sesión',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.tertiary)),
+                      ),
+                    ],
+                  ),
+                ]),
+          ),
         ),
       ),
     );
--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crudtutorial/helper_functions/helper_functions.dart';
+import 'package:crudtutorial/widgets/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class UsersPage extends StatelessWidget {
@@ -5,9 +8,46 @@ class UsersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Users Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Usuarios'),
+      ),
+      drawer: MyDrawer(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Users').snapshots(),
+        builder: (context, snapshot) {
+          //errores
+          if (snapshot.hasError) {
+            displayErrorMessageToUser(
+                context, 'Algo salio mal: $snapshot.error');
+            return Text('Error: ${snapshot.error}');
+          }
+
+          //loading circle
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.data == null) {
+            return const Text('No data');
+          }
+
+          //get all users
+          final users = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              //extraer data
+              final user = users[index];
+
+              return ListTile(
+                title: Text(user['username']),
+                subtitle: Text(user['email']),
+              );
+            },
+          );
+        },
       ),
     );
   }
